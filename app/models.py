3b06f4b1e4541f847fb8638cd07cfe07f65c8b62
benowino7 <bensonowino7@gmail.com>
2020-02-19 15:15:32 +0300

    db image configurations

--- app/models.py
+++ app/models.py
@@ -40,6 +40,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    image_file=db.Column(db.String(50), nullable=False, default='default.jpg')
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
