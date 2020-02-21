fdf2525ca6c79ca67217e4bfbfd6366936efddc4
benowino7 <bensonowino7@gmail.com>
2020-02-21 12:02:21 +0300

    deploying marges from pull requests to heroku

--- app/models.py
+++ app/models.py
@@ -31,6 +31,7 @@ class User(db.Model, UserMixin):
         except:
             return None
         return User.query.get(user_id)
+        
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
